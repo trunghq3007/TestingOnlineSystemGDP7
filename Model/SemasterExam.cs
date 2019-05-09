88d75fd5132c05faed402bbdcca2c826015472b9
nhat <nguyennhatt098@gmail.com>
2019-05-09 15:41:08 +0700

    add 2 model

---
+++ Model/SemasterExam.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+  public  class SemasterExam
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        [Required(ErrorMessage = "status is not null")]
+        public bool Status { get; set; }
+        [Required(ErrorMessage = "semaster is not null")]
+        public string SemasterName { get; set; }
+        [Required(ErrorMessage = "create by is not null")]
+        public string CreateBy { get; set; }
+        public DateTime? StartDate { get; set; }
+
+        public string Code { get; set; }
+        public DateTime? EndDate { get; set; }
+    }
+}

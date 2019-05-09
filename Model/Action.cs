365a45d1764cbb58f9f90d1b82adc7e559ff1f06
hnminhcmc <hoangminhd13ptit@gmail.com>
2019-05-09 15:13:51 +0700

    abcc

---
+++ Model/Action.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class Action
+    {
+        [Key]
+        public int ActionId { get; set; }
+        public string ActionName { get; set; }
+        public string Description { get; set; }
+        public virtual ICollection<RoleAction> RoleActions { get; set; }
+        
+    }
+}

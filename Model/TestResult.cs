0d30c94b906d82f03a1b985007c7343e0cc2fd85
nhat <nguyennhatt098@gmail.com>
2019-05-09 14:47:05 +0700

    add 2 model

--- Model/TestResult.cs
+++ Model/TestResult.cs
@@ -1,12 +1,29 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Model
 {
-    class TestResult
+   public class TestResult
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        [Required(ErrorMessage = "test id is not null")]
+        public int TestId { get; set; }
+        [Required(ErrorMessage = "user id is not null")]
+        public int UserId { get; set; }
+        [Required(ErrorMessage = "question id is not null")]
+        public int QuestionId { get; set; }
+        [Required(ErrorMessage = "answer id is not null")]
+        public int AnwserId { get; set; }
+        [Required(ErrorMessage = "score is not null")]
+        public float Score { get; set; }
+        
+        public string DescriptionName { get; set; }
     }
 }

aa5e26914b842ace863d52c25f65501d39635a18
tiennguyenvan1234597 <nvtien@cmc.com.vn>
2019-05-09 15:09:38 +0700

    them ki thi , kiyhi_nguoithi

---
+++ Model/SemesterExam.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Odbc;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace Model
+{
+    [Table("SemesterExam",Schema ="dbo")]
+    class SemesterExam
+    {
+        [Key]
+        public int ID { get; set; }
+        [Required]
+        public string SemesterName { get; set; }
+        [Required]
+        public DateTime? StartDay { get; set; }
+        [Required]
+        public DateTime? EndDay { get; set; }
+        
+        public string Code { get; set; }
+        public int status { get; set; }
+        public ICollection<SemesterExam_User> semesterExam_Users { get; set; }
+    }
+}

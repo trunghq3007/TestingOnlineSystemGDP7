67eb0dae3708e38f6d60df36dbe868a6b2881789
trieudk <trieudk96@gmail.com>
2019-05-09 13:55:51 +0700

    test push model

---
+++ Model/Tag.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace Model
+{
+    [Table("Tag",Schema ="dbo")]
+    class Tag
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity),Key]
+        public int Id { get; set; }
+        public String Name { get; set; }
+        public String Description { get; set; }
+        public int Status { get; set; } 
+    }
+}

8a4f0f9dcdf18b5c33cecfa3fb8810417fe349ca
hnminhcmc <hoangminhd13ptit@gmail.com>
2019-05-10 15:09:28 +0700

    asb

---
+++ Repository/GroupRepository.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataAccessLayer;
+using Model;
+
+namespace Repository
+{
+    public class GroupRepository : Interfaces.IRepository<Group>, IDisposable
+    {
+        private DBEntityContext context;
+
+        public GroupRepository(DBEntityContext context)
+        {
+            this.context = context;
+        }
+        public int Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Group> Filter(Group t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Group> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Group GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Insert(Group t)
+        {
+            context.Groups.Add(t);
+            return context.SaveChanges();
+        }
+
+
+        public IEnumerable<Group> Search(string searchString)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Update(Group t)
+        {
+            throw new NotImplementedException();
+        }
+
+        private bool disposed = false;
+        public void Dispose(bool disposing)
+        {
+            if (!this.disposed)
+            {
+                if (disposing)
+                {
+                    context.Dispose();
+                }
+            }
+            this.disposed = true;
+        }
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+    }
+}
---
+++ Services/GroupServices.cs
@@ -0,0 +1,56 @@
+﻿using DataAccessLayer;
+using Model;
+using Repository.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Repository;
+
+namespace Services
+{
+    public class GroupServices : Interfaces.IServices<Group>
+    {
+        private IRepository<Group> groupRepository;
+
+        public GroupServices()
+        {
+            groupRepository = new GroupRepository(new DBEntityContext());
+        }
+        public int Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Group> Filter(Group t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Group> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Group GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Insert(Group t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Group> Search(string searchString)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Update(Group t)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

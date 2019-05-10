0959d1d7c0550a611a6de29ab3e881667af93bbf
PhamGioi <phamgioi1202@gmail.com>
2019-05-10 17:04:17 +0700

    create ExamController.cs

---
+++ WebApi/Controllers/ExamController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Newtonsoft.Json;
+using Services;
+using Model;
+
+namespace WebApi.Controllers
+{
+    public class ExamController : Controller
+    {
+        private ExamServices examServices;
+
+        public ExamController()
+        {
+            examServices = new ExamServices();
+        }
+        public string Index()
+        {
+            var result = examServices.GetAll().ToList();
+            return JsonConvert.SerializeObject(result);
+        }
+        [HttpPost]
+        public string Index (Exam exam)
+        {
+            var result = examServices.Insert(exam);
+            return JsonConvert.SerializeObject(result);
+        }
+    }
+}

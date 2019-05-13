883b231765cc0cbb68a83d9dd4cae1bb906be415
hanghoang97 <hanghoangmmt@gmail.com>
2019-05-13 15:33:33 +0700

    kljkl

---
+++ WebApi/Controllers/UserController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using Newtonsoft.Json;
+using Services;
+
+namespace WebApi.Controllers
+{
+    public class UserController : ApiController
+    {
+        private UserSevices services;
+
+        public UserController()
+        {
+            services = new UserSevices();
+        }
+        [HttpGet]
+        // GET: User
+        public string GetUser()
+        {
+            var list = services.GetAll();
+            return JsonConvert.SerializeObject(list);
+        }
+    }
+}

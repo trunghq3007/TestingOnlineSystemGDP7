4265c9e641383d9772dfc783c57ac9f6b70bbced
nhat <nguyennhatt098@gmail.com>
2019-05-13 11:53:39 +0700

    update

---
+++ WebApi/Controllers/TestController.cs
@@ -0,0 +1,35 @@
+﻿using Model;
+using Newtonsoft.Json;
+using Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApi.Controllers
+{
+    public class TestController : Controller
+    {
+        private QuestionServices service;
+
+        public TestController()
+        {
+            service = new QuestionServices();
+        }
+
+        public string Index()
+        {
+            var result = service.GetAll().ToList();
+            return JsonConvert.SerializeObject(result);
+        }
+
+        [HttpPost]
+        public string Index(Question question)
+        {
+            var result = service.Insert(question);
+            return JsonConvert.SerializeObject(result);
+        }
+
+    }
+}

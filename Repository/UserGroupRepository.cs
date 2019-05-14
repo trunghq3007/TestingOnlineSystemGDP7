24df465435d4a32369c37a19b48d88f9d7bd7084
dntinh <dangngoctinh97@gmail.com>
2019-05-14 14:15:13 +0700

    commit Tinh

---
+++ Repository/UserGroupRepository.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.Core.Common.CommandTrees;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using Model;
+using DataAccessLayer;
+namespace Repository
+{
+    public class UserGroupRepository : Interfaces.IGroupRepository<User>
+    {
+        private DBEntityContext context;
+        public UserGroupRepository(DBEntityContext context)
+        {
+            this.context = context;
+        }
+        public int Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int DeleteUserGroup(int iduser, int idgroup)
+        {
+            var del = (from a in context.UserGroups
+                where a.UserId == iduser && a.GroupId == idgroup
+                select a).FirstOrDefault();
+            context.UserGroups.Remove(del);
+            return context.SaveChanges();
+        }
+
+        public IEnumerable<User> Filter(User t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<User> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public User GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<User> GetUserInGroup(int id)
+        {
+            var UserGroup = (from a in context.UserGroups
+                join b in context.Users on a.UserId equals b.UserId
+                where a.GroupId == id
+                select b
+                );
+            return UserGroup.ToList();
+        }
+
+        public int Insert(User t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<User> Search(string searchString)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Update(User t)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ WebApi/Controllers/UserGroupController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+using Newtonsoft.Json;
+using Services;
+namespace WebApi.Controllers
+{
+    public class UserGroupController : ApiController
+    {
+        private UserGroupServices services;
+
+        public UserGroupController()
+        {
+            services = new UserGroupServices();
+        }
+        // GET: UserGroup
+        public string GetUserInGroup(int id)
+        {
+            var result = services.GetUserInGroup(1);
+            return JsonConvert.SerializeObject(result);
+        }
+        [System.Web.Http.HttpDelete]
+        public string Put(int iduser, int idgroup)
+        {
+            var result = services.DeleteUserGroup(iduser,idgroup);
+            return JsonConvert.SerializeObject(result);
+        }
+    }
+}

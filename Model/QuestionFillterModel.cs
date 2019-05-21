9389cef8732efdc018bf3f9a6207c05533b56131
trieudk <trieudk96@gmail.com>
2019-05-21 15:50:33 +0700

    z

--- Model/QuestionFillterModel.cs
+++ Model/QuestionFillterModel.cs
@@ -8,22 +8,22 @@ namespace Model
 {
     public class QuestionFillterModel
     {
-        public int Id { get; set; }
-        public int CategoryId { get; set; }
+        public string Id { get; set; }
+        public string CategoryId { get; set; }
         public string Content { get; set; }
-        public int Level { get; set; }
+        public string Level { get; set; }
         public string Suggestion { get; set; }
         public DateTime? StartDate { get; set; }
         public DateTime? EndDate { get; set; }
-        public int Type { get; set; }
+        public string Type { get; set; }
         public string Media { get; set; }
-        public int Status { get; set; }
+        public string Status { get; set; }
         public string CreatedBy { get; set; }
         public DateTime? CreatedDate { get; set; }
         public string UpdatedBy { get; set; }
         public DateTime? UpdatedDate { get; set; }
-        public int TagsId { get; set; }
-        public int PageIndex { get; set; }
-        public int PageSize { get; set; }
+        public string TagsId { get; set; }
+        public string PageIndex { get; set; }
+        public string PageSize { get; set; }
     }
 }

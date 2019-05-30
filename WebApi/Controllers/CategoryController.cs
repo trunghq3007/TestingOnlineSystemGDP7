e5f4479a3a2db8f394238ccf6ca53abf89c8c056
hnminhcmc <hoangminhd13ptit@gmail.com>
2019-05-30 10:42:47 +0700

    Cors2

--- WebApi/Controllers/CategoryController.cs
+++ WebApi/Controllers/CategoryController.cs
@@ -4,12 +4,14 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using System.Web.Http.Cors;
 using Model;
 using Newtonsoft.Json;
 using Services;
 
 namespace WebApi.Controllers
 {
+    [EnableCors(origins: "*", headers: "*", methods: "*")]
     public class CategoryController : ApiController
     {
         private CategoryService service;

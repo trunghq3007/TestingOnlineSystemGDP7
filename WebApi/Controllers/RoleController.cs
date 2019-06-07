4c7eac5201b2f239caa907ef479af0fe612c3182
hanghoang97 <hanghoangmmt@gmail.com>
2019-06-07 09:04:53 +0700

    commit Role_backend

--- WebApi/Controllers/RoleController.cs
+++ WebApi/Controllers/RoleController.cs
@@ -3,12 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Http;
+using System.Web.Http.Cors;
 using Model;
 using Newtonsoft.Json;
 using Services;
 
 namespace WebApi.Controllers
 {
+    [EnableCors(origins: "*", headers: "*", methods: "*")]
     public class RoleController : ApiController
     {
         private RoleServices services;

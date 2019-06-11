ca50b44967819d76f9dd730ea4cfaca2d0b871f2
Hưng <46187300+11ung@users.noreply.github.com>
2019-06-11 14:00:31 +0700

    Commit Semaster

---
+++ Repository/Interfaces/ISemesterCustomer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository.Interfaces
+{
+    public interface ISemesterCustomer<T1, T2, T3>
+    {
+        IEnumerable<T1> getAll();
+        IEnumerable<T2> getListExam(int id);
+        IEnumerable<T3> getDetailExam(int id);
+    }
+}
---
+++ Services/SemesterCustomerSevice.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataAccessLayer;
+using Model;
+using Repository;
+using Repository.Interfaces;
+using Services.Interfaces;
+
+namespace Services
+{
+    public class SemesterCustomerSevice : Interfaces.ISemesterCustomerService<SemesterExam, Test, Exam>
+    {
+
+        private ISemesterCustomer<SemesterExam, Test, Exam> repository;
+
+        public SemesterCustomerSevice()
+        {
+            repository = new SemesterExamCustomer(new DBEntityContext());
+        }
+
+
+        public IEnumerable<Test> getListExam(int id)
+        {
+            return repository.getListExam(id);
+        }
+
+        public IEnumerable<SemesterExam> getAll()
+        {
+            return repository.getAll();
+        }
+
+        public IEnumerable<Exam> getDetailExam(int id)
+        {
+            return repository.getDetailExam(id);
+        }
+    }
+}

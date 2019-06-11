76f82aa692b5abacb4fdc07da3c04c83384e74b6
ntkien4 <ntkien4@cmc.com.vn>
2019-06-11 18:08:21 +0700

    lovaigvh

---
+++ Model/ViewModel/Result.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model.ViewModel
+{
+   public class Result
+    {
+        public int ID { get; set; }
+        public string TestName { get; set; }
+        public string FullName { get; set; }
+        public string SemesterName { get; set; }
+        public string Email { get; set; }
+
+    }
+}

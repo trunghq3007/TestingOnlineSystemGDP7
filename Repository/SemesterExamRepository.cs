8d9440cd5e1aba3c8e95c1a5f98e11eff7f6b1e4
ntkien4 <ntkien4@cmc.com.vn>
2019-06-18 15:10:51 +0700

    update

--- Repository/SemesterExamRepository.cs
+++ Repository/SemesterExamRepository.cs
@@ -99,7 +99,7 @@ namespace Repository
                 if (semesterExam.status == 2)
                     semesterDetail.status = "Draft";
                 if (semesterExam.status == 0)
-                    semesterDetail.status = "No Public";
+                    semesterDetail.status = "Done";
 
                 var QT = from TR in context.TestResults
                          join U in context.Users on TR.UserId equals U.UserId
@@ -136,7 +136,7 @@ namespace Repository
             Result result = new Result();
             try
             {
-                result.ID = testResult.Id;
+                result.ID = testResult.UserId;
 
                 var query1 = from TR in context.TestResults
                              join T in context.Tests
@@ -162,8 +162,15 @@ namespace Repository
                 result.FullName = query3.FirstOrDefault().FullName;
                 result.Email = query3.FirstOrDefault().Email;
 
+                var query4 = from TR in context.TestResults
+                             where TR.UserId == 2
+                             select TR.Score;
+                result.Score = Convert.ToInt32(query4.Sum());
 
-                result.Score = Convert.ToInt32(testResult.Score);
+
+
+
+            //    result.Score = Convert.ToInt32(testResult.Score);
                 if (result.Score <= 4)
                     result.Category = "Trượt";
                 else
@@ -521,7 +528,7 @@ namespace Repository
             examInformation.TestTime = query.FirstOrDefault().TestTime;
             examInformation.CategoryName = query.FirstOrDefault().Name;
             examInformation.QuestionNumber = query.FirstOrDefault().QuestionNumber;
-            examInformation.TotalScore = 100;
+            examInformation.TotalScore = 10;
             return examInformation;
         }
 
@@ -545,7 +552,7 @@ namespace Repository
                 testResult.QuestionId = item.Question.Id;
                 testResult.UserId = userID;
                 testResult.TestId = testId;
-                if (item.IsTrue == true) testResult.Score = 2;
+                if (item.IsTrue == true) testResult.Score = 1;
                 context.TestResults.Add(testResult);
                 context.SaveChanges();
 
